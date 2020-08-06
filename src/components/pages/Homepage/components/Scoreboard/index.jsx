34f9479c47fd115fea1d3b2e99a329ac5f7ed85c
Chris Sanchez <c.sanch7@gmail.com>
2020-08-05 23:44:46 -0700

    add scoreboard v0.1

--- src/components/pages/Homepage/components/Scoreboard/index.jsx
+++ src/components/pages/Homepage/components/Scoreboard/index.jsx
@@ -1,5 +1,34 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const ScoreboardContainer = styled.section`
+  background: rgb(22, 20, 68);
+  background: linear-gradient(
+    90deg,
+    rgba(22, 20, 68, 1) 0%,
+    rgba(32, 32, 72, 1) 100%
+  );
+`;
+
+const ScoreboardTitle = styled.h1`
+  font-size: 40px;
+  letter-spacing: 4px;
+  text-align: center;
+  margin-top: 0px;
+  margin-bottom: 0px;
+  padding: 24px 0px;
+  background-image: linear-gradient(
+    90deg,
+    rgba(199, 0, 205, 1) 0%,
+    rgba(91, 12, 121, 1) 100%,
+    rgba(22, 20, 68, 1) 100%
+  );
+  background-size: 100%;
+  background-repeat: repeat;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+`;
 
 const Scoreboard = ({ correct }) => {
   const [gamesWon, setGamesWon] = useState(
@@ -16,10 +45,10 @@ const Scoreboard = ({ correct }) => {
   }, [correct]);
 
   return (
-    <div>
-      <div>SCOREBOARD</div>
-      <>Games Won: {gamesWon}</>
-    </div>
+    <ScoreboardContainer>
+      <ScoreboardTitle>SCOREBOARD</ScoreboardTitle>
+      <ScoreboardTitle>Games Won: {gamesWon}</ScoreboardTitle>
+    </ScoreboardContainer>
   );
 };
 
